a34e5f8a43a39752453105a8367d412cd7478b82
JosiasAurel <ndjosiasaurel@gmail.com>
2021-01-09 15:05:49 +0100

    add homepage and route handler

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def _index():
-    return "Hello world"
+    return render_template("index.html")
 
 @app.route("/<name>")
 def _name(name):
